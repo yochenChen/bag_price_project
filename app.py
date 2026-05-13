f053b2c9a99779414590ceddb918cc08ecdc7fab
yochen <>
2026-05-12 21:46:18 -0400

    add refresh csv feature

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template, redirect, url_for
 import pandas as pd
-import os
 
 from scraper import scrape_fashionphile
 
 app = Flask(__name__)
 
-CSV_FILE = "products.csv"
-
 
 def analyze(products):
+
     df = pd.DataFrame(products)
 
     if df.empty:
@@ -34,67 +32,37 @@ def analyze(products):
         .astype(int)
     )
 
-    summary["lowest_price"] = (
-        summary["lowest_price"]
-        .astype(int)
-    )
-
-    summary["highest_price"] = (
-        summary["highest_price"]
-        .astype(int)
-    )
-
-    summary = summary.sort_values(
-        by=["brand", "name"],
-        ascending=[True, True]
-    )
-
     return summary.to_dict(orient="records")
+
+
+cached_rows = []
 
 
 @app.route("/")
 def home():
 
-    # 如果沒有 CSV 就自動爬一次
-    if not os.path.exists(CSV_FILE):
+    global cached_rows
+
+    if not cached_rows:
 
         products = scrape_fashionphile()
 
-        pd.DataFrame(products).to_csv(
-            CSV_FILE,
-            index=False,
-            encoding="utf-8-sig"
-        )
-
-    df = pd.read_csv(CSV_FILE)
-
-    products = df.to_dict(orient="records")
-
-    rows = analyze(products)
+        cached_rows = analyze(products)
 
     return render_template(
         "index.html",
-        rows=rows
+        rows=cached_rows
     )
 
 
 @app.route("/refresh")
 def refresh():
 
-    print("開始重新爬蟲...")
+    global cached_rows
 
     products = scrape_fashionphile()
 
-    df = pd.DataFrame(products)
-
-    # 覆蓋舊 CSV
-    df.to_csv(
-        CSV_FILE,
-        index=False,
-        encoding="utf-8-sig"
-    )
-
-    print("CSV 已更新")
+    cached_rows = analyze(products)
 
     return redirect(url_for("home"))
 
